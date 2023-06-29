7bd3e5c7355cf19233825a92ac1eac9aada86021
MohammadYaser <yasersafi19@outlook.com>
2023-06-29 15:04:08 +0430

    Add displayMobileMenu function in script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Write your functin here
+const toolBar = document.querySelector('.menu-img');
+const mobileNav = document.querySelector('.mobile-menu');
+const cancelX = document.querySelector('.cancel');
+const navList = document.querySelector('#mobile-nav-bar');
+
+function displayMobileMenu() {
+  if (mobileNav.style.display === 'none') mobileNav.style.display = 'block';
+  else mobileNav.style.display = 'none';
+}
