fdaf660d7705de79079135cd32bfb3916a7511c7
MohammadYaser <yasersafi19@outlook.com>
2023-06-30 17:09:10 +0430

    Final update

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const mobileNav = document.querySelector('.mobile-menu');
 const cancelX = document.querySelector('.cancel');
 const navList = document.querySelector('#mobile-nav-bar');
 
-function displayMobileMenu() {
-  if (mobileNav.style.display === 'none') mobileNav.style.display = 'block';
-  else mobileNav.style.display = 'none';
-}
-function hideMobileMenu() {
-  if (mobileNav.style.display === 'block') mobileNav.style.display = 'none';
-  else mobileNav.style.display = 'block';
-}
-function hideMenu() {
+const displayMobileMenu = () => {
+  mobileNav.style.display = (mobileNav.style.display === 'none') ? 'block' : 'none';
+};
+
+const hideMobileMenu = () => {
+  mobileNav.style.display = (mobileNav.style.display === 'block') ? 'none' : 'block';
+};
+
+const hideMenu = () => {
   mobileNav.style.display = 'none';
-}
+};
 
 cancelX.addEventListener('click', hideMobileMenu);
 toolBar.addEventListener('click', displayMobileMenu);
