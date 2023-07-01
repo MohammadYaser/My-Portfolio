3804544962a642194499d8ae8e4515e74eba7ba9
Faiz Mohammad <134736318+the-faizmohammad@users.noreply.github.com>
2023-07-01 11:33:08 +0530

    Add card details in pop.js

--- pop.js
+++ pop.js
@@ -7,4 +7,14 @@ const cardInfos = {
     'Profesional Art Printing Data More',
     'My Recent Works',
   ],
+  image: [
+    'images/ImgPlaceholder0.svg',
+    'images/ImgPlaceholder1.svg',
+    'images/ImgPlaceholder2.svg',
+    'images/ImgPlaceholder3.svg',
+    'images/ImgPlaceholder4.svg',
+    'images/Cancel.svg',
+    'images/liveIcon.svg',
+    'images/githubIcon.svg',
+  ],
   
