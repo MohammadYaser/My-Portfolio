cfa202daf5d39e36c19ef2951a92a0a59749cbf9
Faiz Mohammad <134736318+the-faizmohammad@users.noreply.github.com>
2023-07-01 11:34:06 +0530

    Add card details in pop.js

--- pop.js
+++ pop.js
@@ -17,4 +17,18 @@ const cardInfos = {
     'images/liveIcon.svg',
     'images/githubIcon.svg',
   ],
-  
+  description: [
+    "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  ],
+  technologies: ['CSS', 'HTML', 'Bootstrap', 'Ruby', 'Ruby on Rails'],
+  button: [
+    'See project',
+    'See live',
+    'See source',
+  ],
+  live: 'https://mohammadyaser.github.io/',
+  source: 'https://github.com/MohammadYaser/My-Portfolio',
+};
+
