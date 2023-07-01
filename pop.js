b122b068592cc02b3f6747df0043653ab12ac4b4
Faiz Mohammad <134736318+the-faizmohammad@users.noreply.github.com>
2023-07-01 11:31:34 +0530

    Add card title in pop.js

--- pop.js
+++ pop.js
@@ -1,1 +1,10 @@
 //This will store js file
+const cardInfos = {
+  title: [
+    'Multi-Post Stories',
+    'Data Dashboard Healthcare',
+    'Website Portfolio',
+    'Profesional Art Printing Data More',
+    'My Recent Works',
+  ],
+  
